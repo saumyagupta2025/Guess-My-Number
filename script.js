52f1f07555c52f36ece44a2a923062906edf7ff1
Saumya Gupta <saumyagupta2025@gmail.com>
2023-01-29 14:07:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+"use strict";
+
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
+let secretNumber = Math.trunc(Math.random() * 25) + 1;
+console.log(secretNumber);
+
+let score = 20;
+let highscore = 0;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  if (!guess) {
+    displayMessage("❌ No number!");
+  } else if (guess === secretNumber) {
+    displayMessage("🎉 Correct Number!");
+    document.querySelector(".number").textContent = secretNumber;
+
+    document.querySelector("body").style.backgroundColor = "#60b347";
+
+    document.querySelector(".number").style.width = "30rem";
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? "📈 Too high!!" : "📉 Too low!!");
+      score = score - 1;
+      document.querySelector(".score").textContent = score;
+    } else {
+      displayMessage("👻 You lost the game!");
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 10) + 1;
+  displayMessage("Start guessing...");
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+});
